2282efb9744fbace7b08e01e02195073218685e7
j4sysiak <j4sysiak@gmail.com>
2019-03-06 06:34:04 +0100

    1

---
+++ spring-tutorial-Spring-Dependency-Injection/src/main/java/pl/jaceksysiak/spring/test/Person.java
@@ -0,0 +1,34 @@
+package pl.jaceksysiak.spring.test;
+
+public class Person {
+	
+	private int id;
+	private String name;
+	private int taxId;
+	private Address address;
+	 
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public void setTaxId(int taxId) {
+		this.taxId = taxId;
+	}
+
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public void speak(){
+		System.out.println("Hello I'am a person.");
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", taxId=" + taxId
+				+ ", address=" + address + "]";
+	}
+ 
+}
